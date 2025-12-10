d2e838a3f11d8ba0f4d74893d9f740a62a715667
Maddy <mahammadkc@gmail.com>
2025-12-10 14:57:41 +0530

    commit

--- config.js
+++ config.js
@@ -3,13 +3,19 @@
  */
 
 const PLAYLIST_IDS = [
-  "37i9dQZF1DWVDvBpGQbzXj", // 10's
-  "37i9dQZF1DWZNJXX2UeBij", // 00's
-  "37i9dQZF1DXa2huSXaKVkW", // 90's
-  "37i9dQZF1DX5rOEFf3Iycd", // 80's
-  "37i9dQZF1DX9kVlnA5Si6s", // 70's
-  "37i9dQZF1DXa1eCiO3E6Rr", // 60's
-  "37i9dQZF1DWVClvVUI87z8", // 50's
+  "37i9dQZF1DWVDvBpGQbzXj", // 10's All Out
+  "37i9dQZF1DWZNJXX2UeBij", // 00's All Out
+  "37i9dQZF1DXa2huSXaKVkW", // 90's All Out
+  "37i9dQZF1DX5rOEFf3Iycd", // 80's All Out
+  "37i9dQZF1DX9kVlnA5Si6s", // 70's All Out
+  "37i9dQZF1DXa1eCiO3E6Rr", // 60's All Out
+  "37i9dQZF1DWVClvVUI87z8", // 50's All Out
+  "37i9dQZF1DWVq1SXCH6uFn", // 00's Love Hits
+  "37i9dQZF1DXa6iPZDThhLh", // 90's Love Hits
+  "37i9dQZF1DX3NU3NvyoJUz", // 80's Love Hits
+  "37i9dQZF1DX57WIZsVQSIn", // 70's Love Hits
+  "37i9dQZF1DX6q19gm5UQXx", // 60's Love Hits
+  "37i9dQZF1DX82F21A3zQhD", // 50's Love Hits
 ];
 
 const DIRECTORIES = {
